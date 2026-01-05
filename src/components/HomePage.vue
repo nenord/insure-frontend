9d6dff1fb41b0467d1d6b0aa37f40b3c2a12dd4d
nenord <nradanovic@zoho.com>
2026-01-05 10:55:52 +0000

    small change

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -10,11 +10,14 @@ const token = useTokenStore()
 onMounted(async () => {
   if (token.token.access_token) {
     try {
-      const response = await axios.get('https://insure2-htd9edh8cec2gsf0.canadacentral-01.azurewebsites.net/whoami', {
-        headers: {
-          Authorization: `Bearer ${token.token.access_token}`,
+      const response = await axios.get(
+        'https://insure2-htd9edh8cec2gsf0.canadacentral-01.azurewebsites.net/whoami',
+        {
+          headers: {
+            Authorization: `Bearer ${token.token.access_token}`,
+          },
         },
-      })
+      )
       userinfo.setUserInfo(response.data)
     } catch (error) {
       console.error('Error fetching user info:', error)
@@ -29,7 +32,7 @@ onMounted(async () => {
       <h3>Welcome back {{ userinfo.userInfo.first_name }}!</h3>
     </template>
     <template v-else>
-      <h3>Please log in to continue.</h3>
+      <h3>Please log in to continue. . .</h3>
     </template>
   </div>
 </template>
